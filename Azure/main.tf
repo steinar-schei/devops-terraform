d1445c5d6e7e4488e1ac442320e1f55c194140b0
Steinar Schei <steinar.schei@soprasteira.com>
2021-08-13 10:11:35 +0200

    Added subnet and peering to main

--- Azure/main.tf
+++ Azure/main.tf
@@ -6,18 +6,42 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name = var.resource_group_name
   location = var.location
+  tags = {
+      owner = "steinar.schei@soprasteria.com"
+  }
 }
 
 resource "azurerm_virtual_network" "vnet1" {
   name                = var.vnet1_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet-1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_virtual_network" "vnet2" {
   name                = var.vnet2_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.1.0/24"]
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_virtual_network_peering" "peer1to2" {
+  name                      = "peer1to2"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+}
+
+resource "azurerm_virtual_network_peering" "peer2to1" {
+  name                      = "peer2to1"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
 }
